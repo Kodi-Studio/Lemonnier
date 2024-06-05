e58dbf2b84a169c9243dcf76a9e20946be578eba
Christophe Harel <kodi.webstudio@gmail.com>
2024-06-05 22:06:53 +0200

    add footer cata logo

--- project/public/wp-content/themes/lemonnier/template-homepage.php
+++ project/public/wp-content/themes/lemonnier/template-homepage.php
@@ -40,14 +40,22 @@ declare(strict_types=1);
     <? if (get_post_meta(get_the_ID(), 'show_catas_footer', true) === "1") { ?>
     <section class="section section-full" >
         <div class="section-footer--catalogs" >
-            <div class="max-width-lg">
-                <ul class="footer-catalogs--list"  >
+            <div class="max-width-lg full-vw-width">
+                <div class="text-center footer-logo-cata-container" >
+                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/cata_VLM_full.svg" />
+                </div>
+                <ul class="footer-catalogs--list full-width text-center""  >
+                    
+                    
                     <?php
                         $list = getCatalogs();
                         foreach($list as $cata) {
                     ?>
-                    <li><a href="<?php echo $cata['cata_fichier']; ?>">Télécharger <?php echo $cata['cata_libelle'] ?> </a></li>
+                    
+                        <li><a href="<?php echo $cata['cata_fichier']; ?>"><span class="underline">Télécharger</span> <?php echo $cata['cata_libelle'] ?> </a></li>
+                    
                     <?php } ?>
+                   
                 </ul>
             </div>
         </div>
