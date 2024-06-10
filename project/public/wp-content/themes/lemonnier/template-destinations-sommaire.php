30a517c29de39188addc02cfbf6d0e60055f8e52
Christophe Harel <kodi.webstudio@gmail.com>
2024-06-11 00:03:18 +0200

    add new template file

---
+++ project/public/wp-content/themes/lemonnier/template-destinations-sommaire.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: Template_Destinations_Sommaire
+ */
+
+declare(strict_types=1);
+
+/**
+ * Project: wordpress-skeleton-theme
+ * 
+ * @author Thilo Ratnaweera <thilo.ratnaweera@netbrothers.de>
+ * @copyright © 2024 NetBrothers GmbH.
+ * @license GPLv3
+ */
+
+?>
+<?php get_header(); ?>
+
+<section>
+<center>Page en contruction.</center>
+<br/><br/><br/><br/>
+</section>
+
+
+
+
+<?php get_footer(); ?>
