a4ba44be7caa6b6e9bdfb2c1ce0a3aa402e6b57d
Debian <debian@vps-3f1234f5.vps.ovh.net>
2024-06-11 11:38:19 +0000

    debien commit

---
+++ project/public/wp-content/themes/lemonnier/template-destinations-sommaire.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: Template_Destinations_Sommaire
+ */
+
+declare(strict_types=1);
+
+/**
+ * Project: wordpress-skeleton-theme
+ * 
+ * @author Thilo Ratnaweera <thilo.ratnaweera@netbrothers.de>
+ * @copyright © 2024 NetBrothers GmbH.
+ * @license GPLv3
+ */
+
+?>
+<?php get_header(); ?>
+
+<section>
+    <main>
+        <?php the_content(); ?>
+    </main>
+     
+</section>
+
+
+
+
+<?php get_footer(); ?>
