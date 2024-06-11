c8139c5348831c21f76aeca87210ea5e8a786446
Christophe Harel <kodi.webstudio@gmail.com>
2024-06-11 13:36:53 +0200

    display content into templates desination sommaire

--- project/public/wp-content/themes/lemonnier/template-destinations-sommaire.php
+++ project/public/wp-content/themes/lemonnier/template-destinations-sommaire.php
@@ -16,12 +16,27 @@ declare(strict_types=1);
 ?>
 <?php get_header(); ?>
 
-<section>
-    <main>
-        <?php the_content(); ?>
-    </main>
-     
-</section>
+<section class="section section-full page-header">
+        
+        <img class="image-header" src="<?php echo  get_post_meta(get_the_ID(), 'image_url', true); ?>" />
+        <img class="weaver" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/weaver.svg" />
+        <div class="content-container top-page">
+            <div class="logo-container" >
+                <img class="logo-text" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/Voyages_Le_Monnier_typo_blanc.svg" alt="">
+                <h1 class="page-title" >
+                    Créateur<br /> de voyages
+                </h1>
+            </div>
+        </div>
+    </section>
+    <section class="section" >
+        <div id="content-container" class="content-container page-content" >
+            <main>
+                <?php the_content(); ?>
+            </main>
+            <!-- <?php get_sidebar(); ?> -->
+        </div>
+    </section>
 
 
 
