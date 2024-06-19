f08cecfea197842bcb8c3b577779caaa40f7f6d4
Christophe Harel <kodi.webstudio@gmail.com>
2024-06-19 08:01:14 +0200

    mise en page sommaire destinations

--- project/public/wp-content/themes/lemonnier/template-destinations-sommaire.php
+++ project/public/wp-content/themes/lemonnier/template-destinations-sommaire.php
@@ -16,57 +16,78 @@ declare(strict_types=1);
 ?>
 <?php get_header(); ?>
 
-<section class="section section-full page-header">
+
+<?php
+
+    $pageid = get_the_ID();
+
+    global $wpdb;
+    $query = 	"SELECT * from `travel_type` WHERE travel_type_page_id = $pageid";
+    $type = $wpdb->get_results($query, ARRAY_A );
+
+    $type_id = $type[0]['travel_type_id'];
+
+    $query = 	"SELECT t1.* , t2.* FROM `kdest_travel` t1 LEFT JOIN `travel_discount` t2 ON t1.travel_discount_id = t2.travel_discount_id WHERE t1.travel_type_id = $type_id";
+    $travels = $wpdb->get_results($query, ARRAY_A );
+
+?>
+
+<section class="section section-full page-header page-header--small" style="background-color: <?php echo $type[0]['travel_type_color']  ?>;" >
         
-        <img class="image-header" src="<?php echo  get_post_meta(get_the_ID(), 'image_url', true); ?>" />
+        <!-- <img class="image-header" src="<?php echo  get_post_meta(get_the_ID(), 'image_url', true); ?>" /> -->
         <img class="weaver" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/weaver.svg" />
-        <div class="content-container top-page">
+        <!-- <div class="content-container top-page top-page--small">
             <div class="logo-container" >
                 <img class="logo-text" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/Voyages_Le_Monnier_typo_blanc.svg" alt="">
                 <h1 class="page-title" >
                     Créateur<br /> de voyages
                 </h1>
             </div>
-        </div>
+        </div> -->
     </section>
     <section class="section" >
         <div id="content-container" class="content-container page-content" >
             <main>
-                <?php 
                 
-                    $pageid = get_the_ID();
-
-                    global $wpdb;
-                    $query = 	"SELECT * from `travel_type` WHERE travel_type_page_id = $pageid";
-                    $result = $wpdb->get_results($query, ARRAY_A );
-
-                    $type_id = $result[0]['travel_type_id'];
-
-                    $query = 	"SELECT * from `kdest_travel` WHERE travel_type_id = $type_id";
-                    $travels = $wpdb->get_results($query, ARRAY_A );
-                
-                ?>
-
+                <div style="--color-text:<?php echo $type[0]['travel_type_color']  ?>;" >
+                    <div class="carousel-travels-title">
+                    <h2><?php echo $type[0]['travel_type_title']  ?></h2>
+                    <h3><strong><?php echo $type[0]['travel_type_description']  ?></h3>
+                    </div>
+                </div>
                 <?php the_content(); ?>
-
+                <div class="travel-list">
                 <?php
 
                     foreach( $travels as $travel ) {
                         ?>
-                        <div class="travel_card" >
-                            <h1><?php  echo $travel['travel_title']  ?></h1>
-                            <p>
-                                <?php  echo $travel['travel_description']  ?>
-                            </p>
-                            <p>
+                        <div class="travel-card" >
+                            <div class="type-card--vignette"><img src="<?php  echo $travel['travel_vignette']  ?>" /></div>
+                            <h3 class="--title" ><?php  echo $travel['travel_title']  ?></h3>
+                            <div class="--subtitle">
+                                <?php  echo $travel['travel_subtitle']  ?>
+                            </div>
+                            <div>
+                            <?php
+                            $html = '';
+                            if($travel['travel_discount_id']){
+                                $borderColor = $travel['travel_discount_bgcolor'] == '#transp' ? $travel['travel_discount_color'] : "transparent";
+                                $html .= '<div class="--discount" style="--color-text:'.$travel['travel_discount_color'].'; --color-bg: '.$travel['travel_discount_bgcolor'].'; --border-color:'.$borderColor.'" >'.$travel['travel_discount_libelle'].'</div>';
+                            } else {
+                                $html .= '<div class="--discount" ></div>';
+                            }
+                                echo $html;
+                            ?>
+                            <!-- <p>
                                 <a href="<?php echo get_permalink($travel['travel_page_id'])  , true ?>" >> Voir<a>
-                            </p>
+                            </p> -->
+                            </div>
                         </div>
                         <?php
                     }
 
                 ?>
-
+                </div>
 
             </main>
             <!-- <?php get_sidebar(); ?> -->
