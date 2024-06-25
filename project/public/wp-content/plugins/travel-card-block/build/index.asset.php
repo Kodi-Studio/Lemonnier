aa6f879ceb7ccac537dbaa470df5dc916a8f3782
Christophe Harel <kodi.webstudio@gmail.com>
2024-06-25 22:44:50 +0200

    styles travel cards

--- project/public/wp-content/plugins/travel-card-block/build/index.asset.php
+++ project/public/wp-content/plugins/travel-card-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b0129a2e83c635c05c63');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a55bb3464b2c3a6a199b');
