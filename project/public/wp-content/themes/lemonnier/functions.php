dbee25efb461e08738d64a125c9a25da9fe0b078
Christophe Harel <kodi.webstudio@gmail.com>
2024-07-04 23:20:15 +0200

    Add colors into classic editor

--- project/public/wp-content/themes/lemonnier/functions.php
+++ project/public/wp-content/themes/lemonnier/functions.php
@@ -394,3 +394,62 @@ function enregistrer_champ_upload_image_titre($post_id) {
 // Hooks pour ajouter et enregistrer le champ d'upload d'image titre
 add_action('add_meta_boxes', 'add_field_upload_image_titre');
 add_action('save_post', 'enregistrer_champ_upload_image_titre');
+
+
+
+
+////// ADD colors into classic editor
+function my_mce4_options($init) {
+    $default_colours = '
+        "000000", "Black",
+        "449ea8", "lemonnier",
+        "7D41C2", "Escapades",
+        "C7B600", "sejours",
+        "CC005A", "france",
+        "980071", "AllCircuits",
+        "C41E28", "RedLemonnier",
+        "993300", "Burnt orange",
+        "333300", "Dark olive",
+        "003300", "Dark green",
+        "003366", "Dark azure",
+        "000080", "Navy Blue",
+        "333399", "Indigo",
+        "333333", "Very dark gray",
+        "800000", "Maroon",
+        "FF6600", "Orange",
+        "808000", "Olive",
+        "008000", "Green",
+        "008080", "Teal",
+        "0000FF", "Blue",
+        "666699", "Grayish blue",
+        "808080", "Gray",
+        "FF0000", "Red",
+        "FF9900", "Amber",
+        "99CC00", "Yellow green",
+        "339966", "Sea green",
+        "33CCCC", "Turquoise",
+        "3366FF", "Royal blue",
+        "800080", "Purple",
+        "999999", "Medium gray",
+        "FF00FF", "Magenta",
+        "FFCC00", "Gold",
+        "FFFF00", "Yellow",
+        "00FF00", "Lime",
+        "00FFFF", "Aqua",
+        "00CCFF", "Sky blue",
+        "993366", "Red violet",
+        "FFFFFF", "White",
+        "CC99FF", "Light lavender",
+        "FFCC99", "Peach",
+        "FFFF99", "Light yellow",
+        "CCFFCC", "Pale green",
+        "CCFFFF", "Pale cyan",
+        "99CCFF", "Light sky blue",
+        "CC99CC", "Plum"
+    ';
+
+    $init['textcolor_map'] = '['.$default_colours.']';
+    $init['textcolor_rows'] = 6; // Expand the color grid to 6 rows
+    return $init;
+}
+add_filter('tiny_mce_before_init', 'my_mce4_options');
