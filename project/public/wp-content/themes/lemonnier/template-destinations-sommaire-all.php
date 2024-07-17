22de83f10521c75aa2e2e93fbd8928d4683c3f2c
Christophe Harel <kodi.webstudio@gmail.com>
2024-07-17 21:17:32 +0200

    add filter into all travels page

--- project/public/wp-content/themes/lemonnier/template-destinations-sommaire-all.php
+++ project/public/wp-content/themes/lemonnier/template-destinations-sommaire-all.php
@@ -21,11 +21,32 @@ declare(strict_types=1);
 
     $type_id = $type[0]['travel_type_id'];
 
-    $query = 	"SELECT t1.* , t2.* FROM `kdest_travel` t1 LEFT JOIN `travel_discount` t2 ON t1.travel_discount_id = t2.travel_discount_id WHERE t1.travel_online = 1 ORDER BY t1.travel_display_position DESC, t1.travel_date_a_start ASC, t1.travel_date_b_start ASC, t1.travel_date_c_start ASC";
+    $query = 	"SELECT t1.* , t2.* FROM `kdest_travel` t1 
+                LEFT JOIN `travel_discount` t2 ON t1.travel_discount_id = t2.travel_discount_id 
+                WHERE t1.travel_online = 1 ";
+
+    if(isset($_POST['minprice']) && $_POST['minprice'] != '' && isset($_POST['wishprice'])  && $_POST['wishprice'] == "0" ) {
+
+        $minprice = intval($_POST['minprice']);
+        $query .= " AND t1.travel_price_a_1 >= ".$minprice."";
+    }
+
+    if(isset($_POST['maxprice']) && $_POST['maxprice'] != '' && isset($_POST['wishprice'])  && $_POST['wishprice'] == "0" ) {
+        $maxprice = intval($_POST['maxprice']);
+        $query .= " AND t1.travel_price_a_1 <= ".$maxprice."";
+    }
+
+    $query  .= " ORDER BY t1.travel_display_position DESC, 
+                t1.travel_date_a_start ASC, 
+                t1.travel_date_b_start ASC, 
+                t1.travel_date_c_start ASC";
     $travels = $wpdb->get_results($query, ARRAY_A );
 
 
     $bgHeader = $type[0]['travel_type_color'];
+
+
+    echo $query;
 
 ?>
 
@@ -63,6 +84,24 @@ declare(strict_types=1);
                         <h3><strong><?php echo $type[0]['travel_type_subtitle_page']  ?></strong></h3>
                     </div>
                 </div>
+                <div class="filter-container" >
+                    <form action="#" method="POST" enctype="multipart/form-data" >
+                        <div class="filter-form-container" >
+                            <div class="formob">
+                                <div>RECHERCHE : </div>
+                            </div>
+                            <div class="formob">
+                                <div><input type="number" onkeyup="checkSubmit(event)"  name="minprice" id="minprice" value="<?php echo (isset($_POST['minprice'])) ? $_POST['minprice'] : ''    ?>" placeholder="PRIX MINI" ></div>
+                                <div><input type="number" onkeyup="checkSubmit(event)"  name="maxprice" id="maxprice" value="<?php echo (isset($_POST['maxprice'])) ? $_POST['maxprice'] : ''    ?>" placeholder="PRIX MAXI" ></div>
+                            </div>
+                            <div class="formob">
+                                <div><input type="number" onkeyup="checkSubmit(event)"  name="wishprice" value="0" name="wishprice"  placeholder="PRIX SOUHAITE" ></div>
+                                <div><button type="submit" id="submit" disabled style="background-color: <?php echo $type[0]['travel_type_color']  ?>;" >OK</button></div>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
                 <?php the_content(); ?>
                 <div class="travel-list">
                 <?php
@@ -81,9 +120,16 @@ declare(strict_types=1);
                             if($travel['travel_discount_id']){
                                 $borderColor = $travel['travel_discount_bgcolor'] == '#transp' ? $travel['travel_discount_color'] : "transparent";
                                 $html .= '<div class="--discount" style="--color-text:'.$travel['travel_discount_color'].'; --color-bg: '.$travel['travel_discount_bgcolor'].'; --border-color:'.$borderColor.'" >'.$travel['travel_discount_libelle'].'</div>';
-                            } else {
-                                $html .= '<div class="--discount" ></div>';
                             }
+                            else if($travel['travel_price_a_1']) {
+                                $html .= '<div class="--price" >'.$travel['travel_price_a_1'].'€</div>';
+                            } 
+                            else if($travel['travel_date_alt_text']) {
+                                $html .= '<div class="--text-alt" >'.$travel['travel_date_alt_text'].'</div>';
+                            }
+                            $html .= '<div class="--note" ><div>'.$travel['travel_during_text'].'</div><div class="--note-date" >'.simpleDates($travel['travel_date_a_start'],$travel['travel_date_a_end']).'</div></div>';
+		
+
                                 echo $html;
                             ?>
                             </div>
@@ -142,5 +188,22 @@ declare(strict_types=1);
         </div>
     </section>
     <? } ?>
+
+<script>
+
+    function checkSubmit(e) {
+        if(e.target.value.trim() == 0) {
+            if( parseInt(document.getElementById('minprice').value) > 0  || parseInt(document.getElementById('maxprice').value) > 0 ) {
+                document.getElementById('submit').disabled = false;
+            } else {
+                document.getElementById('submit').disabled = true;
+            }
+        } else {
+            document.getElementById('submit').disabled = false;
+        }
+    }
+
+
+</script>
 
 <?php get_footer(); ?>
